377a9e5091e58f92b9258a4b1fad6d6c47440395
maxs <shepherd.max@gmail.com>
2022-11-22 17:23:47 +0000

    Introduce CragSearchResult and display as Text

--- app/src/main/java/mjms3/github/io/wheretoclimb/MainActivity.kt
+++ app/src/main/java/mjms3/github/io/wheretoclimb/MainActivity.kt
@@ -3,41 +3,77 @@ package mjms3.github.io.wheretoclimb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import mjms3.github.io.wheretoclimb.ui.theme.WhereToClimbTheme
+import java.time.LocalDateTime
+
+data class CragSearchResult(
+    val cragName: String,
+    val percentageMatch: Int,
+    val resultGenerationTime: LocalDateTime
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WhereToClimbTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val materialBlue700 = Color(0xFF1976D2)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("TopAppBar") },
+                            backgroundColor = materialBlue700
+                        )
+                    },
+                    content = { GetSearchResultsContent() },
+                    bottomBar = {
+                        BottomAppBar(backgroundColor = materialBlue700) {
+                            Text("BottomAppBar")
+                        }
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GetSearchResultsContent() {
+    var searchResults = getSearchResults()
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        searchResults.forEach(){
+            result -> GetSearchResultCard(result = result)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WhereToClimbTheme {
-        Greeting("Android")
-    }
+fun GetSearchResultCard(result: CragSearchResult) {
+    Text(result.cragName, fontSize = 36.sp)
+}
+
+fun getSearchResults(): MutableList<CragSearchResult> {
+    val searchResults = mutableListOf<CragSearchResult>(
+        CragSearchResult(
+            cragName = "Stanage",
+            percentageMatch = 80,
+            resultGenerationTime = LocalDateTime.of(2022, 10, 1, 10, 0)
+        ),
+        CragSearchResult(
+            cragName = "Shining Clough",
+            percentageMatch = 2,
+            resultGenerationTime = LocalDateTime.of(2022, 10, 1, 10, 0)
+        )
+    )
+    return searchResults
 }
