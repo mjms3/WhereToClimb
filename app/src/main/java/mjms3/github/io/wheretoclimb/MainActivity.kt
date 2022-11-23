15fffc9bc8feff22bf6832c4bde22ee215142f82
maxs <shepherd.max@gmail.com>
2022-11-23 11:55:08 +0000

    Add colorbar to show percentage match
    Still some things to fix - Canvas size / box drawing isn't correct

--- app/src/main/java/mjms3/github/io/wheretoclimb/MainActivity.kt
+++ app/src/main/java/mjms3/github/io/wheretoclimb/MainActivity.kt
@@ -3,22 +3,30 @@ package mjms3.github.io.wheretoclimb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import mjms3.github.io.wheretoclimb.ui.theme.WhereToClimbTheme
 import java.time.LocalDateTime
+
 
 data class CragSearchResult(
     val cragName: String,
     val percentageMatch: Int,
     val resultGenerationTime: LocalDateTime
 )
+{
+    init{
+        require(this.percentageMatch in 0..100)
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,22 +59,43 @@ fun GetSearchResultsContent() {
     Column(
         modifier = Modifier.verticalScroll(rememberScrollState())
     ) {
-        searchResults.forEach(){
-            result -> GetSearchResultCard(result = result)
+        searchResults.forEach() { result ->
+            GetSearchResultCard(result = result)
         }
     }
 }
 
 @Composable
 fun GetSearchResultCard(result: CragSearchResult) {
-    Text(result.cragName, fontSize = 36.sp)
+    val modifier = Modifier.padding(10.dp)
+    Card(
+        elevation = 10.dp,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Row() {
+            Canvas(modifier = Modifier.size(50.dp)) {
+                val height = size.height
+                drawRect(
+                    size = Size(width = 20f, height = height),
+                    color = getSearchResultBarColor(result),
+                )
+            }
+            Text(text = result.cragName, modifier = modifier)
+        }
+    }
+}
+
+fun getSearchResultBarColor(searchResult: CragSearchResult): Color {
+    val red = kotlin.math.min(2.0f * (1 - searchResult.percentageMatch / 100), 1.0f)
+    val green = kotlin.math.min(2.0f * searchResult.percentageMatch / 100, 1.0f)
+    return Color(red = red, green = green, blue = 0.0f)
 }
 
 fun getSearchResults(): MutableList<CragSearchResult> {
     val searchResults = mutableListOf<CragSearchResult>(
         CragSearchResult(
             cragName = "Stanage",
-            percentageMatch = 80,
+            percentageMatch = 100,
             resultGenerationTime = LocalDateTime.of(2022, 10, 1, 10, 0)
         ),
         CragSearchResult(
